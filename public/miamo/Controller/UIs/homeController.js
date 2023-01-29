e6b3a05c302189b428404e4d9d2354b50fa1bc63
Alexandre Sounalet <alex.sounalet@hotmail.fr>
2023-01-29 18:48:52 +0100

    Phone

--- public/miamo/Controller/UIs/homeController.js
+++ public/miamo/Controller/UIs/homeController.js
@@ -13,6 +13,8 @@ export class homeController extends UiController {
 
     dogPitch = 1;
 
+    phoneUnlocked = false;
+
     /**
      * homeHandler() gère les clics sur al section de la page d'accueil
      * @param {Event} ev Evenement au clic sur la section de la page d'accueil 
@@ -21,15 +23,30 @@ export class homeController extends UiController {
         switch (ev.target.classList[0]) {
             case 'home__doggyz-img':
                 this.dogPitch += 0.02;
+                if (this.dogPitch > 1.3 && !this.phoneUnlocked) {
+                    this.phoneUnlocked = true;
+                    document.querySelector('.home__phone').classList.add('home__phone-dring');
+                    this.audioManager.loadAudioFile('dring');
+                }
+
                 this.audioManager.loadAudioFile('dog', null, this.dogPitch);
                 ev.target.classList.toggle('home__doggyz-crush');
                 if (this.dogPitch > 2) {
                     this.audioManager.loadAudioFile('explosion');
                     ev.target.src = './assets/boom.png';
                     ev.target.className = 'home__doggyz-boom'
+                    ev.target.previousElementSibling.textContent = 'you killed typo dog';
+                    ev.target.nextElementSibling.remove();
                     setTimeout(() => {
                         ev.target.remove();
                     }, 500);
+                }
+                break;
+
+            case 'home__phone':
+                if (this.phoneUnlocked) {
+                    ev.target.classList.remove('home__phone-dring');
+                    this.audioManager.loadAudioFile('call');
                 }
                 break;
         }
